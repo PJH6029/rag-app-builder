9b18c1a722f3de83556c904d62fc22d4e1c380e5
PJH6029 <top231902@naver.com>
2024-08-13 10:23:13 +0900

    Update frontend

--- frontend/util.py
+++ frontend/util.py
@@ -12,11 +12,6 @@ def session_init(session_state):
     if "translated_messages" not in session_state:
         session_state.translated_messages = []
 
-def reset_history(session_state):
-    session_state.messages = []
-    session_state.translated_messages = []
-    msg.info("Chat history cleared.")
-
 def display_chat_history(session_state):
     for message in session_state.messages:
         with st.chat_message(message["role"]):
@@ -32,6 +27,7 @@ def write_chunks(chunks: list[Chunk]):
         st.markdown(f"- chunk {i+1}:")
         st.markdown(f"```\n{chunk.text[:70]}...\n```")
 
+# TODO remove hierarchy
 def _write_source_docs_without_hierarchy(chunks: list[Chunk]):
     combined_chunks = combine_chunks(chunks, attach_url=True)
     
@@ -62,23 +58,22 @@ def write_source_docs(chunks: list[Chunk]):
 
 def write_combined_chunks(combined_chunks: list[CombinedChunks]) -> None:
     for i, combined_chunk in enumerate(combined_chunks):
-        st.markdown(f"## Document: {combined_chunk.doc_meta.get('doc_name', '')}")
+        st.markdown(f"## {combined_chunk.doc_meta.get('doc_name', 'Untitled')}")
         if combined_chunk.doc_meta.get("base_doc_id"):
-            st.write(f"Based on: {combined_chunk.doc_meta.get('base_doc_id')}")
-        st.write(f"Average Score: {combined_chunk.doc_mean_score:.2f}")
+            st.markdown(f"- Based on: {combined_chunk.doc_meta.get('base_doc_id')}")
+        st.markdown(f"- Average Score: {combined_chunk.doc_mean_score:.2f}")
         if combined_chunk.link:
-            st.write(f"URL: [link]({combined_chunk.link})")
-        for chunk in (combined_chunk.chunks):
-            st.markdown(f"### Chunk ({chunk.chunk_id})")
-            st.write(f"#### Score: {chunk.score:.2f}")
-            st.write(f"#### Content")
-            if chunk.chunk_meta.get("page"):
-                try:
-                    st.write(f"Page: {int(chunk.chunk_meta['page'])}")
-                except ValueError:
-                    msg.warn(f"Page number is not an integer: {chunk.chunk_meta['page']}")
-                    
-            st.write(chunk.text)
+            st.markdown(f"- URL: [link]({combined_chunk.link})")
+        
+        for j, chunk in enumerate(combined_chunk.chunks):
+            try:
+                page = int(chunk.chunk_meta.get("page"))
+            except ValueError:
+                msg.warn(f"Page number is not an integer: {chunk.chunk_meta.get('page')}")
+                page = None
+            with st.expander(f"### Chunk {j+1} (page: {page if page else 'N/A'}, score: {chunk.score:.2f})"):
+                with st.container(height=400):
+                    st.markdown(chunk.text)
         
         if i < len(combined_chunks) - 1:
             st.divider()
